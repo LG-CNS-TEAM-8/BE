efca286fd091212aee7cbee13bc0f2cb7ffb0a51
yoonho <nam2660433@gmail.com>
2025-05-16 22:35:47 +0900

    FEAT: RefreshToken으로 AccessToken 재발행 추가
    AuthService createNewAccessToken 메소드 추가
    RefreshToken 엔티티 추가
    RefreshTokenRepository 추가
    RefreshTokenService 추가
    CreateNewAccessTokenResponse 추가
    UserLoginResponse refreshToken 필드 추가

--- src/main/java/com/example/demo/dto/UserLoginResponse.java
+++ src/main/java/com/example/demo/dto/UserLoginResponse.java
@@ -7,5 +7,5 @@ import lombok.Getter;
 @Builder
 public class UserLoginResponse {
     private String accessToken;
-//    private String refreshToken;
+    private String refreshToken;
 }
--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.CreateNewAccessTokenResponse;
 import com.example.demo.dto.UserLoginResponse;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.jwt.JwtTokenProvider;
@@ -16,14 +17,19 @@ import java.util.Map;
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
+    private final RefreshTokenService refreshTokenService;
 
     @Transactional
     public UserLoginResponse login(String email, String password){
         User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("Invalid email"));
         if(!user.getPassword().equals(password))
             throw new IllegalArgumentException("Invalid password");
+
+        String refreshToken = jwtTokenProvider.generateRefreshToken(user.getId());
+        refreshTokenService.saveRefreshToken(refreshToken, user.getId());
         return UserLoginResponse.builder()
                 .accessToken(jwtTokenProvider.generateAccessToken(user.getId()))
+                .refreshToken(refreshToken)
                 .build();
     }
 
@@ -31,5 +37,14 @@ public class AuthService {
     public String signUp(User user){
         userRepository.save(user);
         return "Signup Successful";
+    }
+
+    public CreateNewAccessTokenResponse createNewAccessToken(String refreshToken){
+        if(!jwtTokenProvider.validateToken(refreshToken)){
+            throw new IllegalArgumentException("Invalid refresh token");
+        }
+        Long userId = Long.valueOf(jwtTokenProvider.getuserIdByToken(refreshToken));
+
+        return new CreateNewAccessTokenResponse(jwtTokenProvider.generateAccessToken(userId));
     }
 }
