e707a581222203eea1431447f5249d2c6787ac20
suyeon <93810963+syjungsuyeon@users.noreply.github.com>
2025-05-17 22:18:21 +0900

    feat:add Interest

---
+++ src/main/java/com/example/demo/dto/request/InterestRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class InterestRequestDto {
+    private Long userId;
+    private String name;
+    
+}
