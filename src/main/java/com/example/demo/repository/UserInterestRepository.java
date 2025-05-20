f44a19825408594ea60bde279bae8d2c195a6acc
suyeon <93810963+syjungsuyeon@users.noreply.github.com>
2025-05-20 09:46:17 +0900

    feat: add interest exception

--- src/main/java/com/example/demo/repository/UserInterestRepository.java
+++ src/main/java/com/example/demo/repository/UserInterestRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.domain.Interest;
+import com.example.demo.domain.User;
 import com.example.demo.domain.UserInterest;
 import com.example.demo.domain.UserInterestId;
 
 public interface UserInterestRepository extends JpaRepository<UserInterest, UserInterestId>{
     List<UserInterest> findByUserId(Long userId);
+
+    boolean existsByUserAndInterest(User user, Interest interest);
     
 }
